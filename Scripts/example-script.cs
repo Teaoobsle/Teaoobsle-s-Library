6fc7e215de0e63ccf52da6c3cbcec8dd97075f6c
Teaoobsle <sigmarizzler696912@gmail.com>
2025-03-14 19:27:34 +0000

    sddaad

--- Scripts/example-script.cs
+++ Scripts/example-script.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Enable : MonoBehaviour
+public class example-script : MonoBehaviour
 {
     public GameObject objectEnable;
 
